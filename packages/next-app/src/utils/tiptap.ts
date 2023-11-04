a06a5c639da939575e5ebbae698a0f434cdca98f
G4RDS <account@g4rds.jp>
2023-11-04 18:20:31 +0900

    feat: Render headings as h`level - 1` tags

--- packages/next-app/src/utils/tiptap.ts
+++ packages/next-app/src/utils/tiptap.ts
@@ -4,12 +4,13 @@ import BulletList from '@tiptap/extension-bullet-list';
 import Collaboration from '@tiptap/extension-collaboration';
 import Document from '@tiptap/extension-document';
 import HardBreak from '@tiptap/extension-hard-break';
-import Heading from '@tiptap/extension-heading';
+import Heading, { Level } from '@tiptap/extension-heading';
 import ListItem from '@tiptap/extension-list-item';
 import OrderedList from '@tiptap/extension-ordered-list';
 import Paragraph from '@tiptap/extension-paragraph';
 import Placeholder from '@tiptap/extension-placeholder';
 import Text from '@tiptap/extension-text';
+import { mergeAttributes } from '@tiptap/react';
 
 export const createNoteTitleDocConnection = (noteId: string) => {
   const provider = new HocuspocusProvider({
@@ -47,7 +48,24 @@ export const createNoteContentDocConnection = (noteId: string) => {
   const extensions = [
     Document,
     Text,
-    Heading.configure({
+    Heading.extend({
+      parseHTML() {
+        return this.options.levels.map((level: Level) => ({
+          tag: `h${level + 1}`,
+          attrs: { level },
+        }));
+      },
+      renderHTML({ node, HTMLAttributes }) {
+        const hasLevel = this.options.levels.includes(node.attrs.level);
+        const level = hasLevel ? node.attrs.level : this.options.levels[0];
+
+        return [
+          `h${level + 1}`,
+          mergeAttributes(this.options.HTMLAttributes, HTMLAttributes),
+          0,
+        ] as const;
+      },
+    }).configure({
       levels: [1, 2, 3],
     }),
     BulletList,
