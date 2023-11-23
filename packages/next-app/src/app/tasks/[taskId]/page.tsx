f3e724c2f09d87f26ed436e3fd92431a1a2d35ba
G4RDS <account@g4rds.jp>
2023-11-23 22:14:10 +0900

    feat: Apply new design for task editor

--- packages/next-app/src/app/tasks/[taskId]/page.tsx
+++ packages/next-app/src/app/tasks/[taskId]/page.tsx
@@ -1,23 +1,125 @@
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
+import { prisma } from 'database';
 import { css } from '../../../../styled-system/css';
-import { ParentNoteLink } from './_components/ParentNoteLink';
+import { flex } from '../../../../styled-system/patterns';
+import { NoteIcon } from '../../../components/icons/NoteIcon';
+import { TaskIcon } from '../../../components/icons/TaskIcon';
+import { Header } from '../../_components/Header/Header';
+import { MainContents } from '../../_components/MainContents/MainContents';
 import { TaskEditor } from './_components/TaskEditor/TaskEditor';
 
-export default function Page({ params }: { params: { taskId: string } }) {
+export default async function Page({ params }: { params: { taskId: string } }) {
+  const task = await prisma.task.findUnique({
+    select: {
+      title: true,
+      note: {
+        select: {
+          id: true,
+          title: true,
+        },
+      },
+    },
+    where: {
+      id: params.taskId,
+    },
+  });
+
+  if (!task) {
+    notFound();
+  }
+
   return (
-    <div
-      className={css({
-        w: '100%',
-        height: '100vh',
-        p: '48px',
-      })}
-    >
-      <ParentNoteLink taskId={params.taskId} />
-      <TaskEditor
-        taskId={params.taskId}
-        className={css({
-          mt: 8,
-        })}
+    <>
+      <Header
+        titleEl={
+          <ol
+            className={flex({
+              alignItems: 'center',
+              '& > li': {
+                position: 'relative',
+                pr: 6,
+                color: 'gray.700',
+                fontWeight: 400,
+                _after: {
+                  content: '"/"',
+                  position: 'absolute',
+                  top: '50%',
+                  right: 0,
+                  transform: 'translate(0, -50%)',
+                  w: 6,
+                  color: 'gray.200',
+                  textAlign: 'center',
+                },
+                _last: {
+                  pr: 0,
+                  _after: {
+                    display: 'none',
+                  },
+                },
+              },
+            })}
+          >
+            <li>
+              <Link
+                href="/notes"
+                className={css({
+                  display: 'inline-flex',
+                  alignItems: 'center',
+                  gap: 1,
+                  mr: -2,
+                  py: 1,
+                  px: 2,
+                  borderRadius: '6px',
+                  transition: '0.15s token(easings.easeOut)',
+                  color: 'inherit',
+                  _hover: {
+                    bgColor: 'gray.100',
+                  },
+                })}
+              >
+                <NoteIcon className={css({ w: 4, h: 4, color: 'gray.500' })} />
+                Notes
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={`/notes/${task.note.id}`}
+                className={css({
+                  mx: -2,
+                  py: 1,
+                  px: 2,
+                  borderRadius: '6px',
+                  transition: '0.15s token(easings.easeOut)',
+                  color: 'inherit',
+                  _hover: {
+                    bgColor: 'gray.100',
+                  },
+                })}
+              >
+                {task.note.title}
+              </Link>
+            </li>
+            <li
+              className={css({
+                display: 'flex',
+                alignItems: 'center',
+                gap: 1,
+              })}
+            >
+              <TaskIcon className={css({ w: 4, h: 4, color: 'gray.500' })} />
+              {task.title}
+            </li>
+          </ol>
+        }
       />
-    </div>
+      <MainContents
+        className={css({
+          p: '48px',
+        })}
+      >
+        <TaskEditor taskId={params.taskId} />
+      </MainContents>
+    </>
   );
 }
