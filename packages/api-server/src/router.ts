420a29299873861a08dbd8980fd481efc61a7e2e
G4RDS <account@g4rds.jp>
2023-12-03 21:07:00 +0900

    chore: Remove unused files

--- packages/api-server/src/router.ts
+++
@@ -1,6 +0,0 @@
-import { FastifyPluginAsync } from 'fastify';
-import { getNotes } from './routes/notes';
-
-export const router: FastifyPluginAsync = async (fastify) => {
-  fastify.register(getNotes);
-};
